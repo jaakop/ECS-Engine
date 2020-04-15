320d4af2d5d5b698c55ccf1ed67c97ec7373f698
Jaakko <jaakko@kronogra.fi>
2020-04-15 14:31:52 +0300

    Added transform and hitbox components

---
+++ MGPhysics/Components/RigidBody.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGPhysics.Components
+{
+    public struct RigidBody : IComponent
+    {
+        public Vector HitBox { get; set; }
+    }
+}
---
+++ MGPhysics/Components/Transform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGPhysics.Components
+{
+    public struct Transform : IComponent
+    {
+        public Vector Position { get; set; }
+        public Vector Size { get; set; }
+    }
+}
+    
