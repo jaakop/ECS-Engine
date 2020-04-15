63935ebcfff27a36e4fa3bbf42027d1d17fea276
Jaakko <jaakko@kronogra.fi>
2020-04-15 15:59:33 +0300

    Rewrote the render summary

--- MGPhysics/Systems/RenderSystem.cs
+++ MGPhysics/Systems/RenderSystem.cs
@@ -14,10 +14,9 @@ namespace MGPhysics.Systems
         /// <summary>
         /// Draws sprites on to screen
         /// </summary>
-        /// <param name="sprites">Dictionary of sprites to draw</param>
-        /// <param name="positions">Dictionary of positions to draw the sprites to</param>
-        /// <param name="sizes">Dictionary of sizes for the sprites</param>
-        /// <param name="spriteBatch">Spritebatch to draw with</param>
+        /// <param name="sprites">Dictionary of the drawabel sprites</param>
+        /// <param name="transfroms">Dictionary of transfroms</param>
+        /// <param name="spriteBatch">SpriteBatch to draw the sprites with</param>
         public static void RenderSprites(Dictionary<Entity, Sprite> sprites, Dictionary<Entity, Transform> transfroms, SpriteBatch spriteBatch)
         {
             foreach(KeyValuePair<Entity, Sprite> sprite in sprites)
