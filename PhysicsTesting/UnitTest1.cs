dd4ce099c7ee5a4fc7ed7caf7a11bfe3b81cdb14
Jaakko <jaakko@kronogra.fi>
2020-04-26 17:40:51 +0300

    Added test cases and fixed code

--- PhysicsTesting/UnitTest1.cs
+++ PhysicsTesting/UnitTest1.cs
@@ -49,5 +49,52 @@ namespace PhysicsTesting
             Assert.AreEqual(testVectorA.X - testVectorB.X, sumVector.X, "Vector X coordinate not converted correctly");
             Assert.AreEqual(testVectorA.Y - testVectorB.Y, sumVector.Y, "Vector Y coordinate not converted correctly");
         }
+        [TestMethod]
+        public void VectorNormalization()
+        {
+            Vector testVecotr = new Vector(0, 0);
+            Assert.AreEqual(testVecotr, testVecotr.Normalize(), "VEctor Normalization failed (0,0)");
+            testVecotr = new Vector(10, 0);
+            Assert.AreEqual(new Vector(1,0), testVecotr.Normalize(), "VEctor Normalization failed (1,0)");
+            testVecotr = new Vector(-10, 0);
+            Assert.AreEqual(new Vector(-1,0), testVecotr.Normalize(), "VEctor Normalization failed (-1, 0)");
+            testVecotr = new Vector(0, 10);
+            Assert.AreEqual(new Vector(0, 1), testVecotr.Normalize(), "VEctor Normalization failed (0, 1)");
+            testVecotr = new Vector(0, -10);
+            Assert.AreEqual(new Vector(0, -1), testVecotr.Normalize(), "VEctor Normalization failed (0, -1)");
+        }
+        [TestMethod]
+        public void AngleFromVector()
+        {
+            Vector testVector = new Vector(0,0);
+
+            Assert.AreEqual(90f, Vector.GetAngleFromVector(testVector), "Agnle 90 from vector failed (0,0)");
+
+            testVector = new Vector(1, 0);
+            Assert.AreEqual(0f, Vector.GetAngleFromVector(testVector), "Agnle 0 from vector failed (1,0)");
+
+            testVector = new Vector(0, 1);
+            Assert.AreEqual(90f, Vector.GetAngleFromVector(testVector), "Agnle 90 from vector failed (0,1)");
+
+            testVector = new Vector(-1, 0);
+            Assert.AreEqual(180f, Vector.GetAngleFromVector(testVector), "Agnle 180 from vector failed (-1,0)");
+
+            testVector = new Vector(0, -1);
+            Assert.AreEqual(270f, Vector.GetAngleFromVector(testVector), "Agnle 270 from vector failed (0,-1)");
+
+        }
+        [TestMethod]
+        public void VectorFromAngle()
+        {
+            Vector angleVector = Vector.GetVectorFromAngle(0);
+            Assert.AreEqual(new Vector(1, 0), new Vector((float)Math.Round(angleVector.X, 10), (float)Math.Round(angleVector.Y, 10)), "Vector from angle 0 failed");
+            angleVector = Vector.GetVectorFromAngle(90);
+            Assert.AreEqual(new Vector(0, 1), new Vector((float)Math.Round(angleVector.X, 10), (float)Math.Round(angleVector.Y, 10)), "Vector from angle 90 failed");
+            angleVector = Vector.GetVectorFromAngle(180);
+            Assert.AreEqual(new Vector(-1, 0), new Vector((float)Math.Round(angleVector.X, 10), (float)Math.Round(angleVector.Y, 10)), "Vector from angle 180 failed");
+            angleVector = Vector.GetVectorFromAngle(270);
+            Assert.AreEqual(new Vector(0, -1).X, new Vector((float)Math.Round(angleVector.X, 10), (float)Math.Round(angleVector.Y, 10)).X, "Vector from angle 270 failed");
+            Assert.AreEqual(new Vector(0, -1).Y, new Vector((float)Math.Round(angleVector.X, 10), (float)Math.Round(angleVector.Y, 10)).Y, "Vector from angle 270 failed");
+        }
     }
 }
